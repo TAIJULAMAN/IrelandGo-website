ba3883a5596689ab8041223ed63c61cea875124c
TAIJULAMAN <taijulaman8@gmail.com>
2026-05-15 04:30:30 +0600

    refactor the stoppage api

--- config/envConfig.ts
+++ config/envConfig.ts
@@ -3,11 +3,11 @@
 // export const getBaseUrl = () => url;
 
 
-export const imgUrl = "https://wasiq-backend-v33n.onrender.com";
-export const url = `${imgUrl}/api/v1/`;
-export const getBaseUrl = () => url;
-
-
-// export const imgUrl = "https://api.tourenzo.com";
+// export const imgUrl = "https://wasiq-backend-v33n.onrender.com";
 // export const url = `${imgUrl}/api/v1/`;
 // export const getBaseUrl = () => url;
+
+
+export const imgUrl = "https://api.tourenzo.com";
+export const url = `${imgUrl}/api/v1/`;
+export const getBaseUrl = () => url;
