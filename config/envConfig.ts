a91814a1df26ce488485cacc5fe6ed1ef03202cf
Ahteshamul Hasan <ahteshamulhasan2@gmail.com>
2026-08-26 17:10:17 +0600

    chore: update backend API base URL to production domain

--- config/envConfig.ts
+++ config/envConfig.ts
@@ -2,10 +2,12 @@
 // export const url = `${imgUrl}/api/v1/`;
 // export const getBaseUrl = () => url;
 
+
 // export const imgUrl = "https://wasiq-backend-v33n.onrender.com";
 // export const url = `${imgUrl}/api/v1/`;
 // export const getBaseUrl = () => url;
 
-export const imgUrl = "http://localhost:5000";
+
+export const imgUrl = "https://api.tourenzo.com";
 export const url = `${imgUrl}/api/v1/`;
 export const getBaseUrl = () => url;
