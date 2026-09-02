3a735dd60a6f72dd70bbd80ea2eee21516056d96
Ahteshamul Hasan <ahteshamulhasan2@gmail.com>
2026-09-02 14:44:02 +0600

    weqwer

--- config/envConfig.ts
+++ config/envConfig.ts
@@ -2,12 +2,10 @@
 // export const url = `${imgUrl}/api/v1/`;
 // export const getBaseUrl = () => url;
 
-
 // export const imgUrl = "https://wasiq-backend-v33n.onrender.com";
 // export const url = `${imgUrl}/api/v1/`;
 // export const getBaseUrl = () => url;
 
-
-export const imgUrl = "http://localhost:5000";
+export const imgUrl = "https://api.tourenzo.com";
 export const url = `${imgUrl}/api/v1/`;
 export const getBaseUrl = () => url;
